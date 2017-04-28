a668b2d6e17230ad64a33574972f27ed7b156ad1
hamzah-amin <hamzah.amin26@gmail.com>
2017-04-29 01:09:07 +0500

    login System is initialized

---
+++ eSmallEventMS/src/AdminPortal.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Hamza on 29-Apr-17.
+ */
+public class AdminPortal {
+
+    public AdminPortal(){
+
+        menu();
+
+    }
+
+    private static void menu(){
+
+        System.out.println("Welcome Admin");
+        System.out.printf("");
+        System.out.println("Enter choice to select operations");
+        System.out.println("User Panel");
+        System.out.println("Event Panel");
+
+
+    }
+
+}
---
+++ eSmallEventMS/src/Login.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Hamza on 29-Apr-17.
+ */
+public class Login {
+
+}
